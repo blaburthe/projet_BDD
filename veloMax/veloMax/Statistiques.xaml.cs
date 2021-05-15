a343b2e1ccadb64ec04e30497be42bc87b76136b
Aurélie LEDUC <aurelie.leduc@edu.devinci.fr>
2021-05-15 14:08:39 +0200

    questions 1, 2 et 3 du module stat (design uniquement)

---
+++ veloMax/veloMax/Statistiques.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace veloMax
+{
+    /// <summary>
+    /// Logique d'interaction pour Statistiques.xaml
+    /// </summary>
+    public partial class Statistiques : Window
+    {
+        public Statistiques()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
